2ab0f3834eb4486aea81e1275599e7dc85de0b8f
FrostF0X <dmytro.konowalow@gmail.com>
2019-09-18 21:03:42 +0200

    clarify errors

--- scripts/init_inputs.sh
+++ scripts/init_inputs.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-env
-
 function explode_mirror_repo(){
     local repo=$1
     match="^(ssh:\/\/)?((\w+)@?)?([a-zA-Z0-9\._-]+)(:[0-9]{1,5}+)?(\/|:)?([a-zA-Z\.\/_-]+).git$"
 
     if [[ $(echo $repo | sed -nr "/${match}/p") = '' ]]; then
         echo "Mirror repo value: $repo cannot match (ssh://)?(user@)?(domain)(:port)?(/|:)?(path).git pattern"
-        exit 10;
+        echo "Some examples are:"
+        echo "* git@github.com:syzygypl/mirror-action.git"
+        echo "* ssh://git@git.domain.pl:4422/syzygypl/mirror-action.git"
+        exit 32;
     fi
 
     MIRROR_REPO_USER=$(echo $repo | sed -nr "s/${match}/\3/p")
@@ -24,6 +25,9 @@ function get_input(){
     local var_value=$(eval "echo \"\$${var_name}\"")
     if [[ "${var_value}" = "" ]]; then
         echo "Variable ${name} with env var name $var_name is not defined"
+        echo "Please consider checking:"
+        echo "* Have you used \`with:\` yaml block with proper map parameter \`${name}\` "
+        echo "* Have you used right secret variable - the proper syntax \${{ secrets.{name} }}"
         exit 30;
     fi
     echo "${var_value}"
@@ -33,7 +37,11 @@ function check_ssh_key_content(){
     local key="$1"
     echo "$key" | tr -d '\n\r' | grep -Pz '^-----BEGIN ([A-Z ]+) PRIVATE KEY-----[\w\W]*-----END ([A-Z ]+) PRIVATE KEY-----$'
     if [[ "$?" != "0" ]]; then
-        echo "Key ${key} is not valid"
+        echo "Key ${key} is not valid it should have format: "
+        echo "-----BEGIN {CRYPT} PRIVATE KEY-----"
+        echo "***********************************"
+        echo "***********************************"
+        echo "-----END {CRYPT} PRIVATE KEY-----"
         exit 31;
     fi
 }
